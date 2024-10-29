97cf9eff1feb99ca2f37ad49f1b28f2d5aa48af8
Fernando Sena <fernando.sena@mkdata.com.br>
2024-10-29 19:14:22 -0300

    docs: :memo: create documentation with swagger
    style with redocly

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import apiDocs from './apiDocs';
+import redocly from './redocly';
+import swagger from './swagger';
+import thread from './thread';
+
+export {
+  apiDocs,
+  redocly,
+  swagger,
+  thread,
+};
